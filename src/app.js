2d2c8d359c2e9fcf51c65ecdf8001d24dbac4d18
Jaro <jaroswidnik@interia.pl>
2024-10-30 14:20:43 +0100

    Finished ex3

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 // ./src/app.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import {createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { reducer } from './modules/github'
 import App from './components/App';
 
+const store = createStore(
+    reducer,
+    composeWithDevTools( applyMiddleware(thunk) )
+)
+
+
 const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>
+);
---
+++ src/modules/github/github.types.js
@@ -0,0 +1,3 @@
+export const SET_LOGIN = 'setLogin'
+export const SET_PHRASE = 'setPhrase'
+export const GET_REPOS = 'getRepos'
